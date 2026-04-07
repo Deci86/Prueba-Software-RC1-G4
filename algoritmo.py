cbd8f852939fdf4303ce702dbb9370da5d439ab2
Cristhian Jesús Ricardo Nicho Bruno <cristhiannicho61063070@gmail.com>
2026-04-06 20:41:59 -0500

    Indentacion 2

--- algoritmo.py
+++ algoritmo.py
@@ -8,10 +8,10 @@ def particion(array, menor, mayor):
     array[i + 1], array[mayor] = array[mayor], array[i + 1]
     return i + 1
 def quickSort(array, menor, mayor):
-if menor < mayor:
-    p = particion(array, menor, mayor)
-    quickSort(array, menor, p - 1)
-    quickSort(array, p + 1, mayor)
+    if menor < mayor:
+        p = particion(array, menor, mayor)
+        quickSort(array, menor, p - 1)
+        quickSort(array, p + 1, mayor)
 
 array = [1, 7, 4, 1, 10, 9, -2]
 quickSort(array, 0, len(array) - 1)
