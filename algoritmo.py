9ba98a6d145159852ec989781a895a59d99cf4b6
Cristhian Jesús Ricardo Nicho Bruno <cristhiannicho61063070@gmail.com>
2026-04-06 20:41:18 -0500

    Implementacion algoritmo Quicksort y caso base

--- algoritmo.py
+++ algoritmo.py
@@ -7,3 +7,12 @@ def particion(array, menor, mayor):
             array[i], array[j] = array[j], array[i]
     array[i + 1], array[mayor] = array[mayor], array[i + 1]
     return i + 1
+    def quickSort(array, menor, mayor):
+    if menor < mayor:
+        p = particion(array, menor, mayor)
+        quickSort(array, menor, p - 1)
+        quickSort(array, p + 1, mayor)
+
+array = [1, 7, 4, 1, 10, 9, -2]
+quickSort(array, 0, len(array) - 1)
+print(array)
